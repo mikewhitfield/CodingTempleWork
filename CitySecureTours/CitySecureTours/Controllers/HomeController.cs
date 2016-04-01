8aeaa0bfc7e19978320ef168f14ec595c4a912f6
woody1980 <whitdevx@gmail.com>
2016-04-01 18:52:22 -0500

    Adding File for testing

---
+++ CitySecureTours/CitySecureTours/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using CityToursMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CityToursMVC.Controllers
+{
+    public class HomeController : Controller
+    {   
+        //[Authorize]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+    }
+}
